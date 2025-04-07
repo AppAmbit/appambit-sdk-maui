fc72ad116bc59e7b3a361acbf64f1655f660faab
Luis Hernandez <1307611+luiseduardohd@users.noreply.github.com>
2025-04-07 16:36:25 -0600

    removed changes for endpoint

--- AppAmbitMaui/Models/Logs/Log.cs
+++ AppAmbitMaui/Models/Logs/Log.cs
@@ -40,9 +40,6 @@ internal class Log
     
     [JsonProperty("type")]
     public LogType? Type { get; set; }
-    
-    [JsonProperty("file")] 
-    public string? file { get; set; }
 }
 
 [JsonConverter(typeof(StringEnumConverter))]
