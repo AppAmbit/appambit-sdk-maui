9513bd5273c926b8ed33297b492073269923fdcc
Luis Hernandez <1307611+luiseduardohd@users.noreply.github.com>
2025-04-07 18:00:33 -0600

    Updates for endpoint

--- AppAmbitMaui/Services/APIService.cs
+++ AppAmbitMaui/Services/APIService.cs
@@ -1,6 +1,7 @@
 using System.Net.Http.Headers;
 using System.Reflection;
 using System.Text;
+using AppAmbit.Models.Logs;
 using AppAmbit.Services.Endpoints;
 using AppAmbit.Services.Interfaces;
 using Newtonsoft.Json;
@@ -58,16 +59,66 @@ internal class APIService : IAPIService
         }
     }
 
-    private async Task<HttpContent> SerializeJSONPayload(object payload, IEndpoint endpoint = null)
+    private async Task<HttpContent> SerializePayload(object payload, IEndpoint endpoint = null)
     {
         if (payload == null)
         {
             return null;
         }
         
+        HttpContent content;
+        if (payload is LogTimestamp timestamp)
+        {
+            content = SerializeToMultipartFormDataContent(timestamp);
+        }
+        else
+        {
+            content = SerializeToJSONStringContent(payload);
+        }
+        return content;
+    }
+
+    private static HttpContent SerializeToJSONStringContent(object payload)
+    {
+        
         var data = JsonConvert.SerializeObject(payload);
         var content = new StringContent(data, Encoding.UTF8, "application/json");
         return content;
+    }
+    
+    private HttpContent SerializeToMultipartFormDataContent(object payload)
+    {
+        var formData = new MultipartFormDataContent();
+
+        if (payload == null)
+        {
+            return null;
+        }
+
+        foreach (var property in payload.GetType().GetProperties())
+        {
+            var propertyName = property.Name;
+            var propertyValue = property.GetValue(payload);
+
+            if (propertyName == "file")
+            {
+                var dateFormat = "yyyy-MM-ddTHH_mm_ss_fffZ";
+                var fileName = $"log-{DateTime.Now.ToUniversalTime().ToString(dateFormat)}.txt";
+                var filePath = Path.Combine(FileSystem.AppDataDirectory, fileName);
+                var encodedBytes = Encoding.ASCII.GetBytes(propertyValue as string ?? "");
+                var fileContent = new ByteArrayContent(encodedBytes);
+                fileContent.Headers.ContentType = MediaTypeHeaderValue.Parse("application/octet-stream");
+                formData.Add(fileContent, "file", Path.GetFileName(filePath));
+                continue;
+            }
+            
+            if (propertyValue != null)
+            {
+                var json = JsonConvert.SerializeObject(propertyValue);
+                formData.Add(new StringContent(json, Encoding.UTF8, "application/json"), propertyName);
+            }
+        }
+        return formData;
     }
 
     private string SerializeStringPayload(object payload)
@@ -110,18 +161,18 @@ internal class APIService : IAPIService
                     result = await client.GetAsync(SerializedGetURL(url, payload));
                     break;
                 case HttpMethodEnum.Post:
-                    var payloadJson = await SerializeJSONPayload(payload, endpoint);
+                    var payloadJson = await SerializePayload(payload, endpoint);
                     result = await client.PostAsync(url,payloadJson );
                     break;
                 case HttpMethodEnum.Patch:
                     var requestMessage = new HttpRequestMessage(new HttpMethod("PATCH"), url)
                     {
-                        Content = await SerializeJSONPayload(payload)
+                        Content = await SerializePayload(payload)
                     };
                     result = await client.SendAsync(requestMessage);
                     break;
                 case HttpMethodEnum.Put:
-                    result = await client.PutAsync(url, await SerializeJSONPayload(payload));
+                    result = await client.PutAsync(url, await SerializePayload(payload));
                     break;
                 case HttpMethodEnum.Delete:
                     result = await client.DeleteAsync(url);
