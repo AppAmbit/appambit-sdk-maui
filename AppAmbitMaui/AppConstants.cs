1790e0b276459491bf86c4eb066ddd735b0bad76
Luis Hernandez <1307611+luiseduardohd@users.noreply.github.com>
2025-04-21 09:42:56 -0600

    Update AppConstants.cs

--- AppAmbitMaui/AppConstants.cs
+++ AppAmbitMaui/AppConstants.cs
@@ -5,6 +5,7 @@ internal class AppConstants
     private const string DatabaseFileName = "AppAbmit.db3";
 
     internal const SQLite.SQLiteOpenFlags Flags = SQLite.SQLiteOpenFlags.ReadWrite | SQLite.SQLiteOpenFlags.Create | SQLite.SQLiteOpenFlags.SharedCache;
+
     internal static string DatabasePath => Path.Combine(FileSystem.AppDataDirectory, DatabaseFileName);
     
     internal const string UnknownFileName = nameof(UnknownFileName); 
