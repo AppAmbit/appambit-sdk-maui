382562b73a5b439ea2b25f26291160cc04c5e9fb
roellealku <angel@kavaup.io>
2025-05-22 10:19:12 -0600

    fix: added await to make it asynchronous call

--- AppAmbitMaui/Core.cs
+++ AppAmbitMaui/Core.cs
@@ -79,7 +79,7 @@ public static class Core
         if (!TokenIsValid())
             await InitializeConsumer();
 
-        Crashes.LoadCrashFileIfExists();
+        await Crashes.LoadCrashFileIfExists();
 
         await Crashes.SendBatchLogs();
         await Analytics.SendBatchEvents();
