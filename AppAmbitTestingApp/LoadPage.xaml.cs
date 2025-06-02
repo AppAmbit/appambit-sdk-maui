57fae4807a9dbc1f8d5a34a1c283eb6dae157a2f
roellealku <angel@kavaup.io>
2025-06-02 16:44:17 -0600

    refactor: added relevant progress information for the tester

--- AppAmbitTestingApp/LoadPage.xaml.cs
+++ AppAmbitTestingApp/LoadPage.xaml.cs
@@ -28,57 +28,72 @@ public partial class LoadPage : ContentPage
 
     private async void OnSend500Events(object sender, EventArgs e)
     {
+        eventsLabel.IsVisible = true;
         for (int i = 0; i < 500; i++)
         {
             await Analytics.TrackEvent(_title, new Dictionary<string, string> { { "Test500", "Events" } });
             Debug.WriteLine($"Request Event: {i+1}");
+            eventsLabel.Text = $"Sending event: {i+1} of 500";
             await Task.Delay(100);
         }
+        eventsLabel.IsVisible = false;
         await DisplayAlert("Info", "500 Events generated", "Ok");
     }
 
     private async void OnSend500Logs(object sender, EventArgs e)
     {
+        logsLabel.IsVisible = true;
         for(int i = 0; i < 500; i++)
         {
             await Crashes.LogError(_title);
             Debug.WriteLine($"Request Log: {i+1}");
+            logsLabel.Text = $"Sending Error: {i+1} of 500";
             await Task.Delay(300);
         }
+        logsLabel.IsVisible = false;
         await DisplayAlert("Info", "500 Logs generated", "Ok");
     }
 
     private async void OnSend500StartSession(object sender, EventArgs e)
     {
+        startSessionLabel.IsVisible = true;
         for(int i = 0; i < 500; i++)
         {
             Analytics.ValidateOrInvaliteSession(false);
             await Analytics.StartSession();
             Debug.WriteLine($"Request StartSession: {i+1}");
+            startSessionLabel.Text = $"Sending StartSession: {i+1} of 500";
             await Task.Delay(300);
         }
+        startSessionLabel.IsVisible = false;
         await DisplayAlert("Info", "500 StartSessions requested", "Ok");
     }
 
     private async void OnSend500EndSession(object sender, EventArgs e)
     {
+        endSessionLabel.IsVisible = true;
         for (int i = 0; i < 500; i++)
         {
             Analytics.ValidateOrInvaliteSession(true);
             await Analytics.EndSession();
             Debug.WriteLine($"Request EndSession: {i+1}");
+            endSessionLabel.Text = $"Sending EndSession: {i+1} of 500";
             await Task.Delay(300);
         }
+        endSessionLabel.IsVisible = false;
         await DisplayAlert("Info", "500 EndSessions requested", "Ok");
     }
     private async void OnSend500Tokens(object sender, EventArgs e)
     {
+        tokenLabel.IsVisible = true;
         for (int i = 0; i < 500; i++)
         {
             await Analytics.RequestToken();
             Debug.WriteLine($"Request Token: {i+1}");
+            tokenLabel.Text = $"Sending Token: {i+1} of 500";
             await Task.Delay(300);
         }
+        tokenLabel.IsVisible = false;
         await DisplayAlert("Info", "500 Tokens requested", "Ok");
     }
 }
