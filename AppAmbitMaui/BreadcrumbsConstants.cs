7df708ed3eef3cb05ec06b3dfbfe57359bc00a70
Isaias Canales <jesus@kavaup.io>
2025-11-18 12:18:18 -0600

    fix: Capitalize letter 'A' to "On Appear"

--- AppAmbitMaui/BreadcrumbsConstants.cs
+++ AppAmbitMaui/BreadcrumbsConstants.cs
@@ -7,10 +7,9 @@ namespace AppAmbit
         internal const string onStart   = "On Start";
         internal const string onResume  = "On Resume";
         internal const string onPause   = "On Pause";
-        internal const string onDestroy = "On destroy";
 
         // Pages
-        internal const string onAppear     = "On appear";
+        internal const string onAppear     = "On Appear";
         internal const string onDisappear  = "On Disappear";
 
         // Internet connection
