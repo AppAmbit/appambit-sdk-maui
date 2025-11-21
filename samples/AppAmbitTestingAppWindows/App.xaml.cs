44201083fff43d9f6a41432700828701fed73bd4
Roel Leal <angel@kavaup.io>
2025-11-21 14:19:55 -0600

    refactor: updated macos project into the samples folder

--- samples/AppAmbitTestingAppWindows/App.xaml.cs
+++ samples/AppAmbitTestingAppWindows/App.xaml.cs
@@ -10,6 +10,6 @@ public partial class App : Application
     protected override void OnStartup(StartupEventArgs e)
     {
         base.OnStartup(e);
-        AppAmbitWindows.Start("<YOUR-APPKEY>");
+        AppAmbitSdk.Start("<YOUR-APPKEY>");
     }
 }
