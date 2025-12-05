ae19dafb11b4a489ee114c7cd85788a938cf3172
Roel Leal <angel@kavaup.io>
2025-12-05 16:55:50 -0600

    feat: added avalonia project

---
+++ samples/AppAmbitTestingAppAvalonia/AppAmbitTestingAppAvalonia.Android/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Avalonia;
+using Avalonia.Android;
+using AppAmbitSdkAvalonia;
+
+namespace AppAmbitTestingAppAvalonia.Android;
+
+[Activity(
+    Label = "AppAmbitTestingAppAvalonia.Android",
+    Theme = "@style/MyTheme.NoActionBar",
+    Icon = "@drawable/icon",
+    MainLauncher = true,
+    ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
+public class MainActivity : AvaloniaMainActivity<App>
+{
+    protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
+    {
+        AppAmbitSdk.Start("115521de-1dd4-45a9-9f7e-938189383b99");
+        return base.CustomizeAppBuilder(builder)
+            .WithInterFont();
+    }
+}
---
+++ samples/AppAmbitTestingAppAvalonia/AppAmbitTestingAppAvalonia/App.axaml.cs
@@ -0,0 +1,56 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Data.Core;
+using Avalonia.Data.Core.Plugins;
+using System.Linq;
+using Avalonia.Markup.Xaml;
+using AppAmbitTestingAppAvalonia.ViewModels;
+using AppAmbitTestingAppAvalonia.Views;
+using AppAmbitSdkAvalonia;
+
+namespace AppAmbitTestingAppAvalonia;
+
+public partial class App : Application
+{
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        AppAmbitSdk.Start("115521de-1dd4-45a9-9f7e-938189383b99");
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
+            // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
+            DisableAvaloniaDataAnnotationValidation();
+            desktop.MainWindow = new MainWindow
+            {
+                DataContext = new MainViewModel()
+            };
+        }
+        else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
+        {
+            singleViewPlatform.MainView = new MainView
+            {
+                DataContext = new MainViewModel()
+            };
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+
+    private void DisableAvaloniaDataAnnotationValidation()
+    {
+        // Get an array of plugins to remove
+        var dataValidationPluginsToRemove =
+            BindingPlugins.DataValidators.OfType<DataAnnotationsValidationPlugin>().ToArray();
+
+        // remove each entry found
+        foreach (var plugin in dataValidationPluginsToRemove)
+        {
+            BindingPlugins.DataValidators.Remove(plugin);
+        }
+    }
+}
